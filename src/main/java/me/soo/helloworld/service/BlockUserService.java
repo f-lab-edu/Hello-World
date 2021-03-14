62341576a1a2a5828632ca4e1269bb7f7645f1d0
msugo1 <msugo1@github.com>
2021-03-14 11:52:02 +0900

    [#24] 사용자 차단 해제 기능 구현
    Controller 계층 관련
    
    * BlockUserController
    
    - 사용자 차단 해제 요청을 받을 `unblockUser` 메소드 추가
    
    Service 계층 관련
    
    * BlockUserService
    
    - 사용자 차단 해제에 요청에 대한 비즈니스 로직을 처리 할 `unblockUser`
      메소드 추가
    
    Mapper 계층 관련
    
    * BlockUserMapper
    
    - DB에 차단 해제 내역을 반영 할`deleteBlockUser` 메소드 추가
    
    - DB 관련 행동임을 더 잘 나타내기 위해서 `blockUser ->
      insertBlockUser`로 메소드 이름 변경
    
    테스트 관련
    
    - 친구 차단 해제에 관련된 테스트 작성
    
    - 포스트맨을 활용해 전체적인 테스트

--- src/main/java/me/soo/helloworld/service/BlockUserService.java
+++ src/main/java/me/soo/helloworld/service/BlockUserService.java
@@ -2,6 +2,7 @@ package me.soo.helloworld.service;
 
 import lombok.RequiredArgsConstructor;
 import me.soo.helloworld.exception.DuplicateRequestException;
+import me.soo.helloworld.exception.InvalidRequestException;
 import me.soo.helloworld.mapper.BlockUserMapper;
 import me.soo.helloworld.mapper.FriendMapper;
 import me.soo.helloworld.util.TargetValidator;
@@ -30,16 +31,24 @@ public class BlockUserService {
             case FRIEND_REQUESTED:
             case FRIEND_REQUEST_RECEIVED:
                 friendMapper.deleteFriend(userId, targetId);
-                blockUserMapper.blockUser(userId, targetId);
+                blockUserMapper.insertBlockUser(userId, targetId);
                 break;
             case NONE:
             default:
-                blockUserMapper.blockUser(userId, targetId);
+                blockUserMapper.insertBlockUser(userId, targetId);
                 break;
         }
     }
 
     public boolean isUserBlocked(String userId, String targetId) {
         return blockUserMapper.isUserBlocked(userId, targetId);
+    }
+
+    public void unblockUser(String userId, String targetId) {
+        if (!isUserBlocked(userId, targetId)) {
+            throw new InvalidRequestException("차단되어 있지 않은 사용자를 차단해제 할 수 없습니다. 다시 한 번 확인해주세요.");
+        }
+
+        blockUserMapper.deleteBlockUser(userId, targetId);
     }
 }
