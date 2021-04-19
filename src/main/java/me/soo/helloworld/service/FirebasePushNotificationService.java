00d2629a2cce81e2228d20e5812ce05f1f9e1e6d
msugo1 <msugo1@github.com>
2021-04-19 12:58:43 +0900

    불필요한 import 라인 제거

--- src/main/java/me/soo/helloworld/service/FirebasePushNotificationService.java
+++ src/main/java/me/soo/helloworld/service/FirebasePushNotificationService.java
@@ -1,11 +1,6 @@
 package me.soo.helloworld.service;
 
-import com.google.api.core.ApiFuture;
-import com.google.api.core.ApiFutureCallback;
-import com.google.api.core.ApiFutures;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
@@ -22,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.Executor;
 
 @Slf4j
 @Service
